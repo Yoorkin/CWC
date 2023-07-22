4a2f2ee10ce84c20e7183398ec44a7b832ff81ea
Yoorkin <cyb_yorkin@outlook.com>
2023-07-23 02:57:00 +0800

    update test framework

--- test/Spec.hs
+++ test/Spec.hs
@@ -3,10 +3,10 @@ import Test.QuickCheck
 import Parsing.YmlParser
 import Parsing.Lexer
 import Data.Either
-import Parsing.AST
 import qualified Typing.BidiTyping as Bidi
 import qualified Typing.TypeContext as Context
-import Parsing.AST (Toplevel(Toplevel))
+import qualified Typing.TypedTree as Typed
+import qualified Parsing.AST as AST 
 import System.IO
 import qualified Pipeline as Pipeline
 import System.Directory
@@ -14,61 +14,77 @@ import Parsing.ParserCombinator
 import Debug.Trace
 import Data.List
 
-data TestFiles a = TestFiles [(String, a)] deriving(Eq)
+data PipelineTest = 
+  PipelineTest { 
+    testName :: String,
+    testTokens :: Maybe [Token],
+    testAST :: Maybe AST.Toplevel,
+    testTypedAST :: Maybe (Either Typed.Toplevel [String])
+  }
+  deriving(Eq)
 
-instance Show a => Show (TestFiles a) where
-  show (TestFiles xs) = let f (path,x) = "test file: " ++ path ++ ":\n" ++ show x ++ "\n" 
-            in if null xs then "" else foldl1 (++) (map f xs)
+instance Show PipelineTest where
+  show PipelineTest{testName = n, testTokens = t, testAST = ast, testTypedAST = tast } =
+    "pipeline test '" ++ n ++ "':\n" ++
+    ">>tokens:\n" ++ show t ++
+    ">>ast:\n" ++ show ast ++ 
+    ">>tast:\n" ++ show tast ++
+    "\n" 
+
 readSource :: String -> IO String
 readSource path = do
     file <- openFile path ReadMode
     hGetContents file
 
-lexerTest :: IO (TestFiles (Either (Error Char) [Token]))
-lexerTest = do
-  let folder = "test/parser"
+
+pipelineTest :: FilePath -> IO PipelineTest
+pipelineTest path = do
+    source <- readSource path
+    return $ case tokenize source of 
+        Left err -> PipelineTest { 
+          testName = path, 
+          testTokens = Nothing, 
+          testAST = Nothing, 
+          testTypedAST = Nothing 
+        }
+        Right toks -> 
+          let 
+            ast = Parsing.YmlParser.parse toks
+            ctx = Context.toplevelTyping ast
+            tast = Bidi.checkToplevelType ctx ast
+          in PipelineTest {
+            testName = path,
+            testTokens = Just toks,
+            testAST = Just ast,
+            testTypedAST = Just tast
+          }
+
+batchPipelineTest :: IO [PipelineTest]
+batchPipelineTest = do 
+  let folder = "test/pipeline"
   files <- listDirectory folder
-  let paths = map (\x -> folder ++ "/" ++ x) files
-  let filtered = filter (isSuffixOf ".l") paths
-  sources <- mapM readSource paths
-  let results = map tokenize sources
-  return $ TestFiles (zip filtered results)
-
-parserTest :: IO (TestFiles Toplevel)
-parserTest = do 
-  let folder = "test/parser"
-  files <- listDirectory folder
-  let paths = map (\x -> folder ++ "/" ++ x) files
-  let filtered = filter (isSuffixOf ".l") paths
-  sources <- mapM readSource paths
-  let tokens = map tokenize sources
-  let process = \toks -> case toks of 
-        Left err -> Toplevel [] []
-        Right toks' -> Parsing.YmlParser.parse toks'
-  return $ TestFiles (zip filtered (map process tokens))
-
-
+  let paths = filter (isSuffixOf ".l") (map (\x -> folder ++ "/" ++ x) files)
+  mapM pipelineTest paths
 
 main :: IO ()
 main = do
-  lexerTestResult <- lexerTest
-  parserTestResult <- parserTest
+  pipelineTestResult <- batchPipelineTest
   hspec $ do
     describe "Parser:" $ do
         let process = parse . fromRight [] . tokenize
 
         it "test unhappy parser" $ 
          let result = process "data Tree a = Leaf of a | Node of (Tree a, Tree a)"
-         in result `shouldBe` Toplevel [] []
+         in result `shouldBe` AST.Toplevel [] []
        
         it "test record declaration" $
          let result = process "data CStyleRecord a = CStyleRecord of { leftChild : Tree a, rightChild : Tree b, count : Int }"
-         in result `shouldBe` Toplevel [] []
+         in result `shouldBe` AST.Toplevel [] []
 
         let result = process "fib : int -> int = fun x -> if x <= 2 then x else let r = fib (x - 1) + fib (x - 2) in r"
        
         it "test toplevel binding" $
-          result `shouldBe` Toplevel [] []
+          result `shouldBe` AST.Toplevel [] []
 
         let ctx = Context.toplevelTyping result
         let tast = Bidi.checkToplevelType ctx result
@@ -76,11 +92,8 @@ main = do
         it "test bidi-check of toplevel binding" $
            tast `shouldBe` Right []
 
-        it "test lexer which inputs from 'test/lexer/*.l'" $
-           lexerTestResult `shouldBe` TestFiles []
-
-        it "test parser which inputs from 'test/parser/*.l'" $
-           parserTestResult `shouldBe` TestFiles []
+        it "test pipeline which inputs from 'test/pipeline/*.l'" $
+           pipelineTestResult `shouldBe` []
 
      
 
